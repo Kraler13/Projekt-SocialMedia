9fb95e9d8d9f32631b5854d0d056b443cb32672d
fnowak <filip.nowak7@gmail.com>
2024-08-21 22:39:34 +0200

    Lista postów

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,49 @@
+import './Home.css'
+
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Post from "../components/Post";
+
 const Home = () => {
-    return <h2>Home</h2>;
+    const [posts, setPosts] = useState([])
+
+    const getLatestPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/latest")
+        .then((res)=>{
+            setPosts(res.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+
+    const getNext = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/older-then", {
+            date: posts[posts.length - 1].created_at
+        })
+        .then((res)=>{
+            setPosts(posts.concat(res.data))
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+
+    useEffect(() => {
+        getLatestPosts();
+    }, [])
+    console.log(posts)
+    return (
+        <div className="Home">
+            <div className="postList">
+                {posts.map((post) => {
+                    return <Post post={post} key={post.id} />
+                })}
+                <button onClick={getNext} className='btn loadMore'>Load</button>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
